88235fe1e8f4f279c9a2cc89dad561bb4990d339
zatygrafica <yasseminabudo@gmail.com>
2025-12-06 18:05:12 +0200

    Tradução Fix

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from './en.json';
+import en from './locales/en.json';
 import pt from './locales/pt.json';
 import { normalizeUtf8Translations } from '../utils/encoding';
 
